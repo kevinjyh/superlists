d95a2dc5657e1bbf2e9ebaedb32aa0e7a78e2bcc
Kevin Shen <kevinjyh@gmail.com>
2019-12-28 13:11:58 +0000

    P34:Basic view now returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -5,6 +5,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        # self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -12,9 +13,10 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get('http://localhost:8001/')
 
         # She notices the page title and header mention to-do lists
+        print(self.browser.title)
         self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test!')
 
