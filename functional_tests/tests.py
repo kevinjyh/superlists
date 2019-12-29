f13de8bbdea520756207d7e3b1aacf1ef03ed3d3
Kevin Shen <kevinjyh@gmail.com>
2019-12-29 20:24:32 +0800

    P88:加入新的使用者-Francis後的FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -44,6 +44,8 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
@@ -56,15 +58,36 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-        self.fail('Finish the test!')
+        # 在新的使用者Francis來到網站。
 
-        # She visits that URL - her to-do list is still there.
+        ## 我們使用一個新的瀏覽器工作階段來確保
+        ## Edith的任保資訊都不會被cookies等機制送出
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis造訪首頁。沒有任何Edith的清單的跡象
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis輸入一個新的項目，做出一個新的清單。
+        # 他比Edith無趣...
+        inputbox = self.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis取得他自己的獨一無二URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 同樣的，沒有Edith的清單的任何跡象
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # 他很滿意，都回去睡覺了
+        # self.fail('Finish the test!')
 
         # Satisfied, she goes back to sleep
-
-
-# if __name__ == '__main__':
-#     unittest.main(warnings='ignore')
