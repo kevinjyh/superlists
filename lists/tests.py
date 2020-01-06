628498d66cdf8d25f7d5693c8f5b2bd91d3b2b92
Kevin Shen <kevinjyh@gmail.com>
2020-01-06 06:24:35 +0000

    P111:在urls.py增加add_item，卻尚未於views.py增加add_item前，留做觀看TrackBack之用

--- lists/tests.py
+++ lists/tests.py
@@ -21,21 +21,6 @@ class HomePageTest(TestCase):
         self.assertTrue(html.endswith('</html>'))
 
 
-    # def test_home_page_display_all_list_items(self):
-    #     Item.objects.create(text='itemey 1')
-    #     Item.objects.create(text='itemey 2')
-    #     request = HttpRequest()
-    #     response = home_page(request)
-
-    #     self.assertIn('itemey 1', response.content.decode())
-    #     self.assertIn('itemey 2', response.content.decode())
-
-
-    # def test_home_page_only_saves_items_when_necessary(self):
-    #     request = HttpRequest()
-    #     home_page(request)
-    #     self.assertEqual(Item.objects.count(), 0)
-
 
 class NewListTest(TestCase):
 
@@ -56,6 +41,37 @@ class NewListTest(TestCase):
         )
         new_list = List.objects.first()
         self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
+
+
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
 
 
 class ListAndItemModelsTest(TestCase):
@@ -87,6 +103,7 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
+
 
 
 class ListViewTest(TestCase):
