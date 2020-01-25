3d87994f2ac795d518d7080b3db68ea9858d70ae
kevinjyh <kevinjyh@gmail.com>
2020-01-26 05:39:54 +0800

    quick commit at end of online ch15.3

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,3 +1,4 @@
+from unittest import skip
 from django.utils.html import escape
 from django.urls import resolve
 from django.test import TestCase
@@ -155,3 +156,17 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{list_.id}/')
         self.assertIsInstance(response.context['form'], ItemForm)
         self.assertContains(response, 'name="text"')
+    
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+
+        expected_error = escapte("You've already got this in your list")
+        self.assertContains(responese, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
